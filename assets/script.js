af92adc28f6b31a47adb549df55d224f01382e1d
Clown Supreme <zachary.h.shedd@gmail.com>
2022-11-15 15:45:40 -0800

    Added logic for main page in JS.

---
+++ assets/script.js
@@ -0,0 +1,190 @@
+//Logic for page.
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+var questionsEl = document.getElementById('questions');
+var timerEl = document.getElementById('time');
+var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start');
+var initialsEl = document.getElementById('initials');
+var feedbackEl = document.getElementById('feedback');
+
+function startQuiz() {
+  var startScreenEl = document.getElementById('start-screen');
+  startScreenEl.setAttribute('class', 'hide');
+
+  questionsEl.removeAttribute('class');
+
+  timerId = setInterval(clockTick, 1000);
+
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+
+function getQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+
+  var titleEl = document.getElementById('question-title');
+  titleEl.textContent = currentQuestion.title;
+
+  choicesEl.innerHTML = '';
+
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var choice = currentQuestion.choices[i];
+    var choiceNode = document.createElement('button');
+    choiceNode.setAttribute('class', 'choice');
+    choiceNode.setAttribute('value', choice);
+
+    choiceNode.textContent = i + 1 + '. ' + choice;
+
+    choicesEl.appendChild(choiceNode);
+  }
+}
+
+function questionClick(event) {
+  var buttonEl = event.target;
+
+  if (!buttonEl.matches('.choice')) {
+    return;
+  }
+
+  if (buttonEl.value !== questions[currentQuestionIndex].answer) {
+    time -= 15;
+
+    if (time < 0) {
+      time = 0;
+    }
+
+    timerEl.textContent = time;
+
+    feedbackEl.textContent = 'Wrong!';
+     feedbackEl.textContent = 'Correct!';
+
+  feedbackEl.setAttribute('class', 'feedback');
+  setTimeout(function () {
+    feedbackEl.setAttribute('class', 'feedback hide');
+  }, 1000);
+
+  currentQuestionIndex++;
+  if (time <= 0 || currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+function quizEnd() {
+  clearInterval(timerId);
+
+  var endScreenEl = document.getElementById('end-screen');
+  endScreenEl.removeAttribute('class');
+
+  var finalScoreEl = document.getElementById('final-score');
+  finalScoreEl.textContent = time;
+
+
+  questionsEl.setAttribute('class', 'hide');
+}
+
+function clockTick() {
+  time--;
+  timerEl.textContent = time;
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
+function saveHighscore() {
+  var initials = initialsEl.value.trim();
+
+  if (initials !== '') {
+    var highscores =
+      JSON.parse(window.localStorage.getItem('highscores')) || [];
+    var newScore = {
+      score: time,
+      initials: initials,
+    };
+
+    highscores.push(newScore);
+    window.localStorage.setItem('highscores', JSON.stringify(highscores));
+
+    window.location.href = 'highscores.html';
+  }
+}
+
+function checkForEnter(event) {
+  if (event.key === 'Enter') {
+    saveHighscore();
+  }
+}
+
+submitBtn.onclick = saveHighscore;
+
+startBtn.onclick = startQuiz;
+
+choicesEl.onclick = questionClick;
+
+initialsEl.onkeyup = checkForEnter;
+
+// all questions, choices, and answers
+var questions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+];
+
+// Scores
+function printHighscores() {
+    var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+    highscores.sort(function (a, b) {
+      return b.score - a.score;
+    });
+  
+    for (var i = 0; i < highscores.length; i += 1) {
+      var liTag = document.createElement('li');
+      liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
+  
+      var olEl = document.getElementById('highscores');
+      olEl.appendChild(liTag);
+    }
+  }
+  
+  function clearHighscores() {
+    window.localStorage.removeItem('highscores');
+    window.location.reload();
+  }
+  
+  document.getElementById('clear').onclick = clearHighscores;
+  
+  printHighscores();
